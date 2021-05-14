83552f760165f2e15ff57d666df207c2e2049c94
priyaRaghavan-code <61274816+priyaRaghavan-code@users.noreply.github.com>
2021-05-14 18:31:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,53 +8,141 @@ const email = document.getElementById("email");
 const num = document.getElementById("num");
 
 document.getElementById("submitButton").addEventListener("click", () => {
-    let fnameRes = fname.value
-    let lnameRes = lname.value
-    let deptRes = dept.value
-    console.log(fnameRes)
-    if (fnameRes === null || fnameRes.length <= 1 || fnameRes.length > 15 || !isNumberCheck(fnameRes) || !isSpecialChar(fnameRes)) {
-        console.log("Invalid First Name")
-        document.getElementById("fnameError").style.display = "block"
-    } else {
-        document.getElementById("fnameError").style.display = "none"
-        console.log("Valid First Name");
-    }
+  var decimal =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+  var emailRegEx =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  var phoneNumRegEx = /^\d{10}$/;
+  let fnameRes = fname.value;
+  let lnameRes = lname.value;
+  let deptRes = dept.value;
+  let unameRes = uname.value;
+  let pwdRes = pwd.value;
+  let confirmpwdRes = confirmpwd.value;
+  let emailRes = email.value;
+  let numRes = num.value;
+  let bool = true;
 
-    if (lnameRes === null || lnameRes.length <= 1 || lnameRes.length > 15 || !isNumberCheck(lnameRes) || !isSpecialChar(lnameRes)) {
-        console.log("Invalid First Name")
-        document.getElementById("lnameError").style.display = "block"
-    } else {
-        document.getElementById("lnameError").style.display = "none"
-        console.log("Valid First Name");
-    }
+  if (
+    fnameRes === null ||
+    fnameRes.length <= 1 ||
+    fnameRes.length > 15 ||
+    !isNumberCheck(fnameRes) ||
+    !isSpecialChar(fnameRes) ||
+    !isCapital(fnameRes)
+  ) {
+    document.getElementById("fnameError").style.display = "block";
+    bool = false;
+  } else {
+    document.getElementById("fnameError").style.display = "none";
+  }
 
-    if (dept === null || dept.length <= 1 || lnameRes.length > 15 || !isNumberCheck(lnameRes) || !isSpecialChar(lnameRes)) {
-        console.log("Invalid First Name")
-        document.getElementById("lnameError").style.display = "block"
-    } else {
-        document.getElementById("lnameError").style.display = "none"
-        console.log("Valid First Name");
-    }
+  if (
+    lnameRes === null ||
+    lnameRes.length <= 1 ||
+    lnameRes.length > 15 ||
+    !isNumberCheck(lnameRes) ||
+    !isSpecialChar(lnameRes) ||
+    !isCapital(lnameRes)
+  ) {
+    document.getElementById("lnameError").style.display = "block";
+    bool = false;
+  } else {
+    document.getElementById("lnameError").style.display = "none";
+  }
 
-})
+  if (deptRes === "") {
+    document.getElementById("deptError").style.display = "block";
+    bool = false;
+  } else {
+    document.getElementById("deptError").style.display = "none";
+  }
+
+  if (unameRes === null || unameRes.length <= 1 || unameRes.length > 15) {
+    document.getElementById("unameError").style.display = "block";
+    bool = false;
+  } else {
+    document.getElementById("unameError").style.display = "none";
+  }
+
+  if (!pwdRes.match(decimal)) {
+    document.getElementById("pwdError").style.display = "block";
+    bool = false;
+  } else {
+    document.getElementById("pwdError").style.display = "none";
+  }
+
+  if (pwdRes !== confirmpwdRes) {
+    document.getElementById("confirmpwdError").style.display = "block";
+    bool = false;
+  } else {
+    document.getElementById("confirmpwdError").style.display = "none";
+  }
+
+  if (!emailRes.match(emailRegEx) || !/@gmail\.com$/.test(emailRes)) {
+    document.getElementById("emailError").style.display = "block";
+    bool = false;
+  } else {
+    document.getElementById("emailError").style.display = "none";
+  }
+
+  if (!numRes.match(phoneNumRegEx)) {
+    document.getElementById("numError").style.display = "block";
+    bool = false;
+  } else {
+    document.getElementById("numError").style.display = "none";
+  }
+  
+  if (bool) {
+    alert(
+      "FirstName:" +
+        fnameRes +
+        "\n" +
+        "LastName:" +
+        lnameRes +
+        "\n" +
+        "Department:" +
+        deptRes +
+        "\n" +
+        "UserName:" +
+        unameRes +
+        "\n" +
+        "Password:" +
+        pwdRes +
+        "\n" +
+        "Email:" +
+        emailRes +
+        "\n" +
+        "ContactNo:" +
+        numRes
+    );
+  }
+});
 
 function isNumberCheck(result) {
-    for (let i=0;i<result.length;i++) {
-        if (!isNaN(result[i])) {
-            console.log("Failed at Number")
-            return false
-        }
+  for (let i = 0; i < result.length; i++) {
+    if (!isNaN(result[i])) {
+      console.log("Failed at Number");
+      return false;
     }
-    return true
+  }
+  return true;
 }
 
 function isSpecialChar(result) {
-    const specialChars = ['-', '!', '~', '#', '$']
-    for (let i = 0;i<result.length;i++) {
-        if (specialChars.includes(result[i])) {
-            console.log("Failed at special")
-            return false
-        }
+  const specialChars = ["-", "!", "~", "#", "$"];
+  for (let i = 0; i < result.length; i++) {
+    if (specialChars.includes(result[i])) {
+      console.log("Failed at special");
+      return false;
     }
-    return true
+  }
+  return true;
+}
+
+function isCapital(result) {
+  if (result.charAt(0) !== result.charAt(0).toUpperCase()) {
+    return false;
+  }
+  return true;
 }
