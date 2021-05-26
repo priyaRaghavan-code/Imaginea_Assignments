20422e7df57255b6534b0c6c18d442a83d3fc638
priyaRaghavan-code <priyabhuvanaj99@gmail.com>
2021-05-26 10:34:16 +0530

    Completed

--- dictionary_task/search_for_given_word.rb
+++ dictionary_task/search_for_given_word.rb
@@ -1,80 +1,59 @@
-require 'trie'
-class TrieNode
-  attr_accessor :is_end
-  TrieNode Trie[]
-  def def initialize(is_end)
-    @is_end = true
-  end
-
-  def TrieNode
-    Trie = new TrieNode[256]
-    0.upto(256) do |i|
-      Trie[i] = nil
-    end
-    @is_end = false
-  end
-end
-
 class Node
+  attr_accessor :is_end,:Trie
   def initialize()
+    @is_end = false
+    @Trie = Array.new(26,nil)
   end
-
-  def insert_node(TrieNode root, string)
-    TrieNode temp = root
-    0.downto(string.length) do |i|
-      if temp.Trie[string[i]] == nil
-        temp.Trie[string[i]] = TrieNode.new()
-      end
-      temp = temp.Trie[string[i]]
-    end
-    temp.@is_end = true
-  end
-
-  def print_suggested_words(TrieNode root,string)
-    if root.@is_end == nil
-      p result + " "
-    end
-    0.downto(256) do |i|
-      if root.Trie[i] != nil
-        result += i.chr 
-        print_suggested_words(root.Trie[i], result)
-        result = result[0..result.length-2]
-      end
-    end
-  end
-
-  def check_string_is_present(TrieNode root, key)
-    0.downto(key.lenght) do |i|
-      if root.Trie[key[i]] == nil
-        print_suggested_words(root, key[0..i])
-      end
-      root = root.Trie[key[i]]
-    end
-    if root.is_end == true
-      return true
-    else
-      print_suggested_words(root, key)
-    end
-    return false
-  end
-
-
 end
-# Json_array = { :A => ["apple", "app", "aple", "an", "aeroplane", "and", "alphabet", "again", "assure", "alone"], 
-#                :B => ["banned", "banana", "buffalo", "bush", "bus", "band", "burst", "bull", "bind", "bound"],
-#                :C => ["cat", "can", "cape", "carnivel", "carrot", "car", "cast", "cut", "cute", "cutest","curl"] }
-# string_to_search = gets
 
-string_words = ["hi", "hind","hello","how","here","her"]
-key = "hifi"
-TrieNode root = TrieNode.new()
+def insert_node(root,word)
+  for i in 0..word.length - 1
+    if word[i] != nil && root.Trie[word[i].ord - 97] == nil
+      root.Trie[word[i].ord - 97] = Node.new()
+    end
+    root = root.Trie[word[i].ord - 97]
+  end
+  root.is_end = true
+end
+
+def print_suggested_words(root, result)
+  if root.is_end == true
+    puts "Do you mean? " + result
+  end
+  for i in 0..26 - 1
+    if root.Trie[i] != nil
+      result += (i+97).chr 
+      print_suggested_words(root.Trie[i], result)
+      result = result[0..result.length-2]
+    end
+  end
+end
+
+def check_string_is_present(root, key)
+  for i in 0..key.length - 1
+    if root.Trie[key[i].ord - 97] == nil
+      print_suggested_words(root, key[0..i - 1])
+      return false
+    end
+    root = root.Trie[key[i].ord - 97]
+  end
+  if root.is_end == true
+    return true
+  else
+    print_suggested_words(root, key)
+  end
+  return false
+end
+
+string_words = ["hi", "hind","hello","how","here","her", "google","apple","App","application","bus","bush","burst","Appointed"]
+key = "appointed"
+root = Node.new()
 
 string_words.each do |word|
-  insert_node(root,word)
+  insert_node(root,word.downcase)
 end
 
-if check_string_is_present(root, key)
-  p "yes " + key +" is present"
+# print_suggested_words(root,key)
+if check_string_is_present(root, key.downcase)
+ p  key +" is present"
 end
-
-
